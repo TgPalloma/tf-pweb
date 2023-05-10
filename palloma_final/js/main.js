a9483d9d2b03699b5be3aa7d965e18ae49eb5296
Palloma Muller <palloma.muller@hotmail.com>
2023-05-10 13:55:21 -0300

    v0.2
    - Divisão da página de cadastro
    - Colocação dos campos
    - Pre-formatação do campo CNPJ no main.js
    - Ícone de titulo da página

---
+++ palloma_final/js/main.js
@@ -0,0 +1,19 @@
+//expressao regular
+//var cnpj = "32456789000123";
+
+//replace
+// /^2(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/
+// "$1.$2.$3/$4-$5"
+
+//console.log(cnpj.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5"));
+
+function formatarCNPJ(mascara, documento) {
+    let i = documento.value.length;
+    let saida = '#';
+    let texto = mascara.substring(i);
+    while (texto.substring(0, 1) != saida && texto.length ) {
+      documento.value += texto.substring(0, 1);
+      i++;
+      texto = mascara.substring(i);
+    }
+  }
