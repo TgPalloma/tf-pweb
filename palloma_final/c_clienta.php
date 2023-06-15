fb947f9df8b36be6a20e6a4f22ceb0975a158c6b
Palloma Muller <126531769+TgPalloma@users.noreply.github.com>
2023-06-14 21:55:43 -0300

    Add files via upload

---
+++ palloma_final/c_clienta.php
@@ -0,0 +1,47 @@
+<?php
+
+$razaoSocial = $_POST["razao_social"];
+$nomeFantasia = $_POST["nome_fantasia"];
+$cnpj = $_POST["cnpj"];
+$formatoJuridico = $_POST["tipo_empresa"];
+
+$endereco = $_POST["endereco"];
+$bairro = $_POST["bairro"];
+$cidade = $_POST["cidade"];
+$estado = $_POST["estado"];
+
+$contato1 = $_POST["nomeDoContato_1"];
+$contato2 = $_POST["nomeDoContato_2"];
+$telefone1 = $_POST["telefone1"];
+$telefone2 = $_POST["telefone2"];
+$email1 = $_POST["email1"];
+$email2 = $_POST["email2"];
+
+//====== Banco de dados ======//
+
+$server = "localhost";
+$db= "aula";
+$user = "root";
+$senha = "";
+
+$server = "localhost";
+$db= "aula";
+$user = "root";
+$senha = "";
+
+$con = mysqli_connect($server,$user,$senha,$db);
+if(!$con){
+    die("erro ao conectar no banco de dados " .
+            mysqli_connect_error());
+}
+
+require('conecta_banco.php');
+
+$sql = "INSERT INTO clientes (razaosocial, nomefantasia, cnpj, formatojuridico,
+endereco, bairro, cidade, estado, 
+contato1, contato2, telefone1, telefone2, email1, email2)
+VALUES ('".$razaoSocial."','".$nomeFantasia."','".$cnpj."','".$formatoJuridico."','"
+.$endereco."','".$bairro."','".$cidade."','".$estado."','".$contato1."','".$contato2."','"
+.$telefone1."','".$telefone2."','".$email1."','".$email2."')";
+
+?>
