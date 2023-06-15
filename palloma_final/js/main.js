7e99d64852697770bfd255745b854aa551439cbe
Dienifer Menegotto <133290932+dienifermenegotto@users.noreply.github.com>
2023-06-14 22:02:55 -0300

    Update
    Realizada validação dos campos vazios em js.

--- palloma_final/js/main.js
+++ palloma_final/js/main.js
@@ -18,4 +18,75 @@ function formatarCNPJ(mascara, documento) {
     }
   }
 
+ function validaCadastro(){
 
+  var razaoSocial = document.getElementById("razao_social").value;
+  var nomeFantasia = document.getElementById("nome_fantasia").value;
+  var cnpj = document.getElementById("cnpj").value;
+  var formatoJuridico = document.getElementById("formato_juridico").value;
+  var endereco = document.getElementById("endereco").value;
+  var bairro = document.getElementById("bairro").value;
+  var cidade = document.getElementById("cidade").value;
+  var estado = document.getElementById("estado").value;
+  var telefone1 = document.getElementById("telefone_1").value;
+  var email1 = document.getElementById("email_1").value;
+  var contato1 = document.getElementById("contato_1").value;
+
+  if (razaoSocial == ""){
+      alert("Campo Razão Social vazio!");
+    document.getElementById("razao_social").focus();
+    return;  
+  } 
+  if (nomeFantasia == ""){
+    alert("Campo Nome Fantasia vazio!");
+    document.getElementById("nome_fantasia").focus();
+    return;
+  }
+  if (cnpj == ""){
+    alert("Campo CNPJ vazio!");
+    document.getElementById("cnpj").focus();
+    return;
+  }
+  if (formatoJuridico == ""){
+  alert("Campo Formato Juridico vazio!");
+    document.getElementById("formato_juridico").focus();
+    return;
+  }
+  if (endereco == ""){
+    alert("Campo Endereço vazio!");
+    document.getElementById("endereco").focus();
+    return;
+  }
+  if (bairro == ""){
+    alert("Campo Bairro vazio!");
+    document.getElementById("bairro").focus();
+    return;
+  }
+  if (cidade == ""){
+    alert("Campo Cidade vazio!");
+    document.getElementById("cidade").focus();
+    return;
+  }
+  if (estado == ""){
+    alert("Campo Estado vazio!");
+    document.getElementById("estado").focus();
+    return;
+  }
+  if (telefone1 == ""){
+    alert("Campo Telefone vazio!");
+    document.getElementById("telefone_1").focus();
+    return;
+    }
+  if (email1 == ""){
+      alert("Campo E-mail vazio!");
+      document.getElementById("email_1").focus();
+      return;
+  }
+  if (contato1 == ""){
+      alert("Campo contato vazio!");
+      document.getElementById("contato_1").focus();
+      return;
+  } else {
+      windows.location.href = "cadastra.php";
+    }
+}
